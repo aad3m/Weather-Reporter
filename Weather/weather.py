5740750951defac88bc40885163047693040a037
Ali <36553593+aad3m@users.noreply.github.com>
2023-12-16 13:18:06 -0600

    Update weather.py

--- Weather/weather.py
+++ Weather/weather.py
@@ -5,6 +5,7 @@ API_KEY = '19fc96ed1a1daebe000e661170eff8d6'
 
 city = input('Enter a city name: ')
 city = city.title()
+
 # Can be modified to weather program of your liking
 base_url = f'http://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}'
 weather_data = requests.get(base_url).json()
