30cc8a49963ca092a35740851b7c1e8a53ebee58
Ali <36553593+aad3m@users.noreply.github.com>
2023-12-16 13:15:39 -0600

    Update weather.py
    Prints out weather information, still need to convert from Kelvin to Fahrenheit and Celsius

--- Weather/weather.py
+++ Weather/weather.py
@@ -1,12 +1,28 @@
 import requests
-from pprint import pprint
 
 API_KEY = '19fc96ed1a1daebe000e661170eff8d6'
 
 city = input('Enter a city name: ')
-
-base_url = 'http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + API_KEY
-
+city = city.title()
+base_url = f'http://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}'
 weather_data = requests.get(base_url).json()
 
-pprint(weather_data)
+
+# Extract specific information
+print(f'The weather in {city} currently:')
+conditions = weather_data['weather'][0]['description'].title()
+current_temp = weather_data['main']['temp']
+max_temp = weather_data['main']['temp_max']
+min_temp = weather_data['main']['temp_min']
+feels_like = weather_data['main']['feels_like']
+wind_speed = weather_data['wind']['speed']
+visibility = weather_data.get('visibility', 'N/A')
+
+# Print the information
+print(f"Conditions: {conditions}")
+print(f"Current Temperature: {current_temp} K")
+print(f"Today's High: {max_temp} K")
+print(f"Today's Low: {min_temp} K")
+print(f"Feels Like: {feels_like} K")
+print(f"Wind Speed: {wind_speed} m/s")
+print(f"Visibility: {visibility} meters")
