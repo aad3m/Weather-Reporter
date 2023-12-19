d115ff26950af8fc6b0ece175876cdceef6d46ca
Ali <36553593+aad3m@users.noreply.github.com>
2023-12-18 22:51:27 -0600

    Update weather.py
    Major changes to code, including options for Fahrenheit and Celsius, Miles and Kilo, replaced code with functions to simplify work

--- Weather/weather.py
+++ Weather/weather.py
@@ -1,31 +1,87 @@
 import requests
+print('Welcome to the weather reporter!')
 
-# Can be modified to your own API
+# Get data from openweathermap.org
+def get_weather_data(api_key, city):
+    base_url = f'http://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}'
+    response = requests.get(base_url)
+
+    # Checks if able to connect to weather data
+    if response.status_code == 200:
+        return response.json()
+    else:
+        print(f"Error: Unable to fetch weather data. Status code: {response.status_code}")
+        return None
+
+
+# convert speed to user decided unit
+def convert_speed(value, unit):
+    if unit == 'Miles':
+        return value * 2.23694, 'mph'
+    elif unit == 'Kilo':
+        return value * 3.6, 'km/h'
+    else:
+        return value, unit
+
+# convert visibility to user decided unit
+def convert_visibility(value, unit):
+    if unit == 'Miles':
+        value = value * 0.000621371
+        unit = 'mi'
+    elif unit == 'Kilo':
+        value = value * 0.001
+        unit = 'km'
+    return value, unit
+
+
+# convert temp to user decided unit
+def convert_temperature(value, unit):
+    if unit == 'Fahrenheit':
+        return (value - 273.15) * 9 / 5 + 32, 'F'
+    elif unit == 'Celsius':
+        return value - 273.15, 'C'
+    else:
+        return value, unit
+
+
+# diplay all the info
+def display_weather_info(city, weather_data, user_units, user_temp):
+    degrees_symbol = "\u00b0"
+
+    conditions = weather_data['weather'][0]['description'].title()
+    current_temp = weather_data['main']['temp']
+    max_temp = weather_data['main']['temp_max']
+    min_temp = weather_data['main']['temp_min']
+    feels_like = weather_data['main']['feels_like']
+    wind_speed = weather_data['wind']['speed']
+    visibility = weather_data.get('visibility', 'N/A')
+
+    wind_speed, wind_unit = convert_speed(wind_speed, user_units)
+    visibility, visibility_unit = convert_visibility(visibility, user_units)
+
+    current_temp, temp_unit = convert_temperature(current_temp, user_temp)
+    max_temp, _ = convert_temperature(max_temp, user_temp)
+    min_temp, _ = convert_temperature(min_temp, user_temp)
+    feels_like, _ = convert_temperature(feels_like, user_temp)
+
+    print(f'The weather in {city} currently:')
+    print(f"Conditions: {conditions}")
+    print(f"Current Temperature: {current_temp:.0f} {degrees_symbol}{temp_unit}")
+    print(f"Today's High: {max_temp:.0f} {degrees_symbol}{temp_unit}")
+    print(f"Today's Low: {min_temp:.0f} {degrees_symbol}{temp_unit}")
+    print(f"Feels Like: {feels_like:.0f} {degrees_symbol}{temp_unit}")
+    print(f"Wind Speed: {wind_speed:.0f} {wind_unit}")
+    print(f"Visibility: {visibility:.0f} {visibility_unit}")
+
+
+# Running code:
 API_KEY = '19fc96ed1a1daebe000e661170eff8d6'
-
 city = input('Enter a city name: ')
-city = city.title()
+user_units = input('Miles or Kilo: ')
+user_units = user_units.title()
+user_temp = input('Celsius or Fahrenheit: ')
+user_temp = user_temp.title()
 
-# Can be modified to weather program of your liking
-base_url = f'http://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}'
-weather_data = requests.get(base_url).json()
-
-
-# Extract specific information
-print(f'The weather in {city} currently:')
-conditions = weather_data['weather'][0]['description'].title()
-current_temp = weather_data['main']['temp']
-max_temp = weather_data['main']['temp_max']
-min_temp = weather_data['main']['temp_min']
-feels_like = weather_data['main']['feels_like']
-wind_speed = weather_data['wind']['speed']
-visibility = weather_data.get('visibility', 'N/A')
-
-# Print the information
-print(f"Conditions: {conditions}")
-print(f"Current Temperature: {current_temp} K")
-print(f"Today's High: {max_temp} K")
-print(f"Today's Low: {min_temp} K")
-print(f"Feels Like: {feels_like} K")
-print(f"Wind Speed: {wind_speed} m/s")
-print(f"Visibility: {visibility} meters")
+weather_data = get_weather_data(API_KEY, city)
+if weather_data:
+    display_weather_info(city, weather_data, user_units, user_temp)
